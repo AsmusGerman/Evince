97627e9298aff507da4cc1948252ef4e43771053
segoviaal <alesegovia07@gmail.com>
2020-01-20 08:59:45 -0300

    Mejoras en la vista del chofer

--- src/app/driver/recorridos-list/recorridos-list.component.ts
+++ src/app/driver/recorridos-list/recorridos-list.component.ts
@@ -1,15 +1,108 @@
 import { Component, OnInit } from "@angular/core";
 import {MatInputModule} from '@angular/material';
+import { Viaje } from 'src/app/core/model/viaje';
 
 const RECORRIDOS = [
-    {
-        "id" : "A-C",
-        "viajes": ["A-B","B-C"]
+  {
+    "id" : "A-C",
+    "viajes": [
+        {
+        "id" : "Viaje1Rec1",
+        "cantPasajeros" : 43,
+        "diaSalida" : "30/06/2020",
+        "diaLlegada" : "30/06/2020",
+        "horaEstipuladaSalida" : "18:00",
+        "horaEstipuladaLlegada" : "22:00",
+        "horaRealSalida" : "",
+        "horaRealLlegada" : "",
+        "recorridoId" : "A-C",
+        "actual" : false,
+        "trayecto" : 
+        {
+          "trayectoId" : "AB",
+          "terminalOrigen" : "A",
+          "terminalDestino" : "B"
+        }
+        },
+        {
+        "id" : "Viaje2Rec1",
+        "diaSalida" : "30/06/2020",
+        "diaLlegada" : "30/06/2020",
+        "cantPasajeros" : 40,
+        "horaEstipuladaSalida" : "22:00",
+        "horaEstipuladaLlegada" : "23:00",
+        "horaRealSalida" : "",
+        "horaRealLlegada" : "",
+        "recorridoId" : "A-C",
+        "actual" : true,
+        "trayecto" : 
+        {
+          "trayectoId" : "BC",
+          "terminalOrigen" : "B",
+          "terminalDestino" : "C"
+        }
+        }
+    ]
+  },
+  {
+    "id" : "F-I",
+    "viajes": [
+      {
+        "id" : "Viaje1Rec2",
+        "diaSalida" : "01/07/2020",
+        "diaLlegada" : "01/07/2020",
+        "cantPasajeros" : 50,
+        "horaEstipuladaSalida" : "08:00",
+        "horaEstipuladaLlegada" : "09:00",
+        "horaRealSalida" : "",
+        "horaRealLlegada" : "",
+        "recorridoId" : "F-I",
+        "actual" : false,
+        "trayecto" : 
+        {
+          "trayectoId" : "FG",
+          "terminalOrigen" : "F",
+          "terminalDestino" : "G"
+        }
       },
       {
-        "id" : "F-I",
-        "viajes": ["F-G","G-H","H-I"]
-    }
+        "id" : "Viaje2Rec2",
+        "diaSalida" : "01/07/2020",
+        "diaLlegada" : "01/07/2020",
+        "cantPasajeros" : 54,
+        "horaEstipuladaSalida" : "09:00",
+        "horaEstipuladaLlegada" : "11:00",
+        "horaRealSalida" : "",
+        "horaRealLlegada" : "",
+        "recorridoId" : "F-I",
+        "actual" : false,
+        "trayecto" : 
+        {
+          "trayectoId" : "GH",
+          "terminalOrigen" : "G",
+          "terminalDestino" : "H"
+        }
+      },
+      {
+        "id" : "Viaje3Rec2",
+        "diaSalida" : "01/07/2020",
+        "diaLlegada" : "01/07/2020",
+        "cantPasajeros" : 46,
+        "horaEstipuladaSalida" : "09:00",
+        "horaEstipuladaLlegada" : "13:00",
+        "horaRealSalida" : "",
+        "horaRealLlegada" : "",
+        "recorridoId" : "F-I",
+        "actual" : false,
+        "trayecto" : 
+        {
+          "trayectoId" : "HI",
+          "terminalOrigen" : "H",
+          "terminalDestino" : "I"
+        }
+      }
+    ]
+}
 ];
 
 @Component({
@@ -21,11 +114,24 @@ const RECORRIDOS = [
 export class RecorridosListComponent implements OnInit {
   //public iDisplayedColumns: string[] = ["code", "last", "state","subscription", "analyze"];
     public iDataSource = RECORRIDOS;
-  
+    public viajeActual:Viaje;
+
+   getViajeActual() {
+     //let viajeActual:Viaje;
+     this.iDataSource.filter(e=>{
+       e.viajes.filter(ee=> {
+        if(ee.actual==true)
+          this.viajeActual=ee;
+       });
+      });
+      //return viajeActual;
+}
+
   constructor() {}
 
   ngOnInit() {
-      console.log("asd");
+    this.getViajeActual();
+    console.log(this.getViajeActual());
   }
 
 }
